dbad6d112b6f1960c8dc28ed957870a9caa7330e
ARUN-MOHAN-USHA <126851308+ARUN-MOHAN-USHA@users.noreply.github.com>
2023-12-21 14:19:34 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import Layout from "./components/Layout/Layout";
 
 function App() {
+  axios.defaults.withCredentials = true;
+  axios.post('https://final-project-api-phi.vercel.app')
   return <Layout/>;
 }
 
